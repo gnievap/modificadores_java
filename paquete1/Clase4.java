ef34b949b17a1d50c6bf81378a472ac742b817c2
Gaby Nieva <gaby.nieva@outlook.com>
2025-06-26 10:59:16 -0600

    Changes to Clase4.java

--- paquete1/Clase4.java
+++ paquete1/Clase4.java
@@ -2,6 +2,7 @@ package paquete1;
 
 public class Clase4 {
     private boolean x;
+    private int y;
 
     public boolean getX() {
         return x;
@@ -10,13 +11,18 @@ public class Clase4 {
     public void setX(boolean x) {
         this.x = x;
     }
+    
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 
     @Override
     public String toString() {
-        return "Clase4 [x=" + x + "]";
+        return "Clase4 [x=" + x + " y= " + y + "]";
     }
-    
 
-    
-    
 }
