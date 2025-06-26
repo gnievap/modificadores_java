81a0ba4bb9dd0a103d26fcd69d032f901eccc079
Gaby Nieva <gaby.nieva@outlook.com>
2025-06-26 10:43:10 -0600

    Adding Clase4.java

---
+++ paquete1/Clase4.java
@@ -0,0 +1,22 @@
+package paquete1;
+
+public class Clase4 {
+    private boolean x;
+
+    public boolean getX() {
+        return x;
+    }
+
+    public void setX(boolean x) {
+        this.x = x;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase4 [x=" + x + "]";
+    }
+    
+
+    
+    
+}
